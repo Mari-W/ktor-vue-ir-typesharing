a36ef152eb55f539ab769187b99204cd70fda2b1
Marius Weidner <marius.weidner@ptwdosimetry.com>
2021-01-14 20:02:38 +0100

    fix dependency resolution error on new clone

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,63 +18,6 @@ kotlin {
     }
 }
 
-val node = tasks.register<Copy>("node") {
-    copy {
-        from("$buildDir/processedResources/js/main")
-        include("package.json")
-        into("$buildDir/libs/shared-types/")
-    }
-    copy {
-        from("$buildDir/compileSync/kotlin")
-        into("$buildDir/libs/shared-types/")
-        rename { name -> name.replace("${rootProject.name}-shared", "index") }
-    }
-}
-
-val jvm = tasks.register<Copy>("jvm") {
-    delete("$buildDir/libs/kt-jvm")
-    copy {
-        from("$projectDir/src/main/kotlin")
-        include("**/*.kt")
-        into("$buildDir/libs/kt-jvm/")
-    }
-
-    ant.withGroovyBuilder {
-        "replaceregexp"(
-            "match" to "@JsExport",
-            "replace" to "// @JsExport - not available nor needed on JVM",
-            "flags" to "g"
-        ) {
-            "fileset"(
-                "dir" to "$buildDir/libs/kt-jvm/", "includes" to "**/*.kt"
-            )
-        }
-    }
-    ant.withGroovyBuilder {
-        "replaceregexp"(
-            "match" to "@ExperimentalJsExport",
-            "replace" to "// @ExperimentalJsExport - not available nor needed on JVM",
-            "flags" to "g"
-        ) {
-            "fileset"(
-                "dir" to "$buildDir/libs/kt-jvm/", "includes" to "**/*.kt"
-            )
-        }
-    }
-    // add package to file
-    ant.withGroovyBuilder {
-        "replaceregexp"(
-            "match" to "package ",
-            "replace" to "package ${project.group}.types.",
-            "flags" to "g"
-        ) {
-            "fileset"(
-                "dir" to "$buildDir/libs/kt-jvm/", "includes" to "**/*.kt"
-            )
-        }
-    }
-}
-
 tasks {
     "build" {
         doLast {
@@ -143,7 +86,6 @@ tasks {
                 }
             }
         }
-        finalizedBy(":client:addPackage")
-        finalizedBy(":server:addTypes")
+        finalizedBy(":client:addPackage", ":server:addTypes")
     }
 }
